d4baa9461362789b6cde93ec07f1221dcfd7e8c2
Leonid Logvinov <logvinov.leon@gmail.com>
2016-03-10 17:15:00 +0100

    Initial complete implementation

--- Main.hs
+++ Main.hs
@@ -1,11 +1,41 @@
 import System.Environment
-
-(<$>) :: (Functor f) => (a -> b) -> f a -> f b
-f <$> x = fmap f x
+import Control.Applicative
+import Data.List
+import MyArray
 
 readContents :: [String] -> IO String
 readContents [] = getContents
 readContents (filename:_) = readFile filename
 
+type Vertex = Int
+type Incidence = (Vertex, [Vertex])
+type Graph = Array Vertex [Vertex]
+
+readIncidence :: String -> Incidence
+readIncidence s = (v, neighbours)
+  where v:neighbours = read <$> words s
+
+readGraph :: [String] -> Graph
+readGraph ls = array (1, length ls) $ sort $ map readIncidence ls
+
+graphSize :: Graph -> Int
+graphSize = length.elems
+
+type Used = Array Vertex Bool
+dfs :: Graph -> Vertex -> Used -> Used
+dfs g v used =
+  if used ! v then used
+  else foldr (dfs g) (used // [(v, True)]) (g ! v)
+
+reachable :: Graph -> Vertex -> [Vertex]
+reachable g v = let gs = graphSize g in
+                let used = array (1, gs) (zip [1..gs] (cycle [False])) in
+                let usage = zip [1..gs] (elems $ dfs g v used) in
+                fst <$> (filter snd usage)
+
+solve :: String -> String
+solve c = let g = readGraph $ lines c in
+          show $ reachable g 1
+
 main :: IO ()
-main = do getArgs >>= readContents >>= putStrLn
+main = solve <$> (getArgs >>= readContents) >>= putStrLn
