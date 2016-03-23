b92ce59c16c874a9ab8a189813c75fb8b6573b5a
Leonid Logvinov <logvinov.leon@gmail.com>
2016-03-23 08:43:39 +0100

    Initial AVL implementation

--- MyArray.hs
+++ MyArray.hs
@@ -1,57 +1,92 @@
 {-# LANGUAGE RecordWildCards #-}
-module MyArray (Ix, range, rangeSize,
-               Array, listArray, (!), elems, array, update, (//)) where
+module MyArray (Ix(..), listArray, (!), elems, update, array, (//)) where
+
 import MyArrayIx
 
-data Node i e = Node {key::i, value::e, left::Node i e, right::Node i e}
-              | Nil deriving (Show, Ord, Eq)
+data Node i e = Node {key::i,
+                     value::e,
+                     left::Node i e,
+                     right::Node i e,
+                     height::Int} | Nil deriving (Show, Ord, Eq)
 
-listToNode :: Ix i => [(i, e)] -> Node i e
-listToNode [] = Nil
-listToNode x = Node {key=k, value=v, left=listToNode l, right = listToNode r}
-  where (l, (k, v):r) = splitAt (length x `div` 2) x
+singleton :: Ix i => (i, e) -> Node i e
+singleton (i, e) = Node{key=i, value=e, left=Nil, right=Nil, height=1}
+
+getHeight :: Ix i => Node i e -> Int
+getHeight Nil = 0
+getHeight Node{..} = height
+
+balance :: Ix i => Node i e -> Int
+balance Nil = 0
+balance Node{..} = getHeight right - getHeight left
+
+fixHeight :: Ix i => Node i e -> Node i e
+fixHeight Nil = Nil
+fixHeight n@Node{..} = n{height=succ $ maximum $ fmap getHeight [left, right]}
+
+rLeft :: Ix i => Node i e  -> Node i e
+rLeft p@Node{left=l, right=q@Node{left=m, right=r}} =
+  fixHeight q{left=fixHeight p{left=l, right=m}, right=r}
+rLeft _ = error "Invalid left rotation"
+
+rRight :: Ix i => Node i e -> Node i e
+rRight p@Node{left=q@Node{left=l, right=m}, right=r} =
+  fixHeight q{left=l, right=fixHeight p{left=m, right=r}}
+rRight _ = error "Invalid right rotation"
+
+rebalance :: Ix i => Node i e -> Node i e
+rebalance n@Node{..}
+  | b == 2 = rLeft n{right=if balance right < 0 then rRight right else right}
+  | b == -2 = rRight n{left=if balance left > 0 then rLeft left else left}
+  | otherwise = n
+   where b = balance n
+rebalance Nil = error "Invalid rebalance"
+
+add :: Ix i => (i, e) -> Node i e -> Node i e
+add e Nil = singleton e
+add e@(k, v) n@Node{..}
+  | key < k = rebalance.fixHeight $ n{right=add e right}
+  | key > k = rebalance.fixHeight $ n{left=add e left}
+  | otherwise = n{value=v}
 
 find :: Ix i => Node i e -> i -> Maybe e
 find Nil _ = Nothing
-find Node {..} i
+find Node{..} i
   | i < key = find left i
   | i > key = find right i
   | otherwise = Just value
 
 values :: Ix i => Node i e -> [e]
 values Nil = []
-values Node {..} = values left ++ value : values right
+values Node{..} = values left ++ [value] ++ values right
 
-modify :: Ix i => i -> e -> Node i e -> Node i e
-modify _ _ Nil = error "Key not found"
-modify i e n@Node {key=k, left=l, right=r}
-  | i < k = n {left=modify i e l}
-  | i > k = n {right=modify i e r}
-  | otherwise = n {value=e}
-
-data Array i e = Array {bounds::(i, i), tree::Node i e} deriving (Show)
+data Array i e = Array{bounds::(i, i), tree::Node i e} deriving (Show)
 
 listArray :: Ix i => (i, i) -> [e] -> Array i e
-listArray (l, r) es = Array {bounds = (l, r), tree = t}
-  where t = listToNode $ zip (range (l, r)) (es ++ repeat undefined)
+listArray b es = Array{bounds=b, tree=foldr add Nil (zip (range b) es)}
 
 (!) :: Ix i => Array i e -> i -> e
-(!) Array {tree=t} i = fromJust $ find t i
+(!) Array{..} i
+  | inRange bounds i = fromJust $ find tree i
+  | otherwise = error "Index out of defined range"
 
 elems :: Ix i => Array i e -> [e]
-elems = values . tree
+elems Array{..} = values tree
 
 array :: Ix i => (i, i) -> [(i, e)] -> Array i e
-array b@(l, r) es = Array {bounds=b, tree=foldr (uncurry modify) acc es}
-  where acc = tree $ listArray (l, r) []
+array b es
+  | all (inRange b) (map fst es) = Array{bounds=b, tree=foldr add Nil es}
+  | otherwise = error "Index out of range"
 
 update :: Ix i => i -> e -> Array i e -> Array i e
-update i e a@Array {tree=t} = a {tree=modify i e t}
+update i e a@Array{..}
+  | inRange bounds i = a{tree=add (i, e) tree}
+  | otherwise = error "Index out of defined range"
 
 (//) :: Ix i => Array i e -> [(i, e)] -> Array i e
-(//) = foldr $ uncurry update
+(//) a@Array{..} es
+  | all (inRange bounds) (map fst es) = a{tree=foldr add tree es}
 
-fromJust :: Maybe a -> a
-fromJust (Just a) = a
-fromJust Nothing = error "Nothing here"
-
+fromJust :: Maybe e -> e
+fromJust (Just e) = e
+fromJust Nothing = error "Element is undefined"
